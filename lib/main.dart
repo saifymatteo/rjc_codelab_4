d73e5d5630c21602511ea0ef2f6ddf6a09aa59e8
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-05-11 21:41:33 +0800

    start of project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: SplashPage(),
+    ),
+  );
+}
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
